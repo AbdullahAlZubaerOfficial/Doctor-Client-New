e5fdb28c6dd2a3a40384a38f66aa1d686b892919
Zubaer3002 <your-email@example.com>
2025-07-20 17:26:44 +0600

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Doc-House-New/', // Firebase hosting এর জন্য base path সঠিকভাবে সেট করা
+  base: '/', // ✅ Vercel er jonno eta thakbe
 })
